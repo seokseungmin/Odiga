07d956a1d7dcb6ff2c8e998c966c3ca4cac172ab
seokseungmin <seoksmin@gmail.com>
2025-04-20 04:39:05 +0900

    chore(husky): 커밋 메시지 설정

---
+++ src/main/java/com/snh/odiga/OdigaApplication.java
@@ -0,0 +1,13 @@
+package com.snh.odiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdigaApplication.class, args);
+	}
+
+}
